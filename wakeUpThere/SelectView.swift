419c814386058f7ddbd96d750c09adc70bf977eb
danyk20 <daniel.kosc@gmail.com>
2022-06-05 14:38:21 -0500

    Transport type buttons added

--- wakeUpThere/SelectView.swift
+++ wakeUpThere/SelectView.swift
@@ -11,21 +11,32 @@ import CoreLocationUI
 
 struct SelectView: View {
     @StateObject private var mapModel = MapViewModel()
+    @State private var selectedTransportType = "Choose mean of transport:"
 
     var body: some View {
-        VStack(alignment: .center, spacing: 50) {
+        VStack(alignment: .center, spacing: 30) {
             Text("WUT")
                 .font(.largeTitle)
                 .fontWeight(.bold)
             
-            Text("Choose mean of transport:")
+            Text(selectedTransportType)
                 .fontWeight(.bold)
             
             ForEach(TransportType.allCases, id: \.rawValue) { vehicle in
-                        TransportView(transportType: vehicle)
+                Button(action: {
+                    selectedTransportType = vehicle.rawValue
+                }, label: {
+                    RoundedRectangle(cornerRadius: 35)
+                        .fill(Color.white)
+                        .shadow(radius: 10)
+                        .overlay(TransportView(transportType: vehicle)
+                            .accentColor(Color.black))
+                }
+                )
                     }
             
             MapView()
+                .ignoresSafeArea()
         }
         .font(.title)
         
