e04de6cb9b8727628a7e5b383166ddbcfce4aeba
danyk20 <daniel.kosc@gmail.com>
2022-07-19 15:09:01 -0500

    removed unneccessary files

---
+++ wakeUpThere/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  wakeUpThere
+//
+//  Created by Daniel Košč on 19/07/2022.
+//
+
+import Foundation
+
+class Constants{
+    public static let flightRadarAPIKey = "4e879a6157msh8d3d1a4c002c00ep14db1ejsn495a0bed8861"
+}
